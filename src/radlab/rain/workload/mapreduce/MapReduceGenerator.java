611eae6a3571be522f4a3ce503ccbc9848aa5245
Aaron Beitch <aaron.beitch@gmail.com>
2010-03-18 16:30:43 -0700

    Added skeleton mapreduce files.

---
+++ src/radlab/rain/workload/mapreduce/MapReduceGenerator.java
@@ -0,0 +1,44 @@
+package radlab.rain.workload.mapreduce;
+
+import radlab.rain.Generator;
+import radlab.rain.Operation;
+import radlab.rain.ScenarioTrack;
+
+public class MapReduceGenerator extends Generator {
+
+	public MapReduceGenerator(ScenarioTrack track) {
+		super(track);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getCycleTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getThinkTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Operation nextRequest(int lastOperation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/radlab/rain/workload/mapreduce/MapReduceOperation.java
@@ -0,0 +1,32 @@
+package radlab.rain.workload.mapreduce;
+
+import radlab.rain.Generator;
+import radlab.rain.IScoreboard;
+import radlab.rain.Operation;
+
+public class MapReduceOperation extends Operation {
+
+	public MapReduceOperation(boolean interactive, IScoreboard scoreboard) {
+		super(interactive, scoreboard);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void execute() throws Throwable {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prepare(Generator generator) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
