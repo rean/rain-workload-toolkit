d373f65eaaf872f92015fb665e756feb908c118c
Aaron Beitch <aaron.beitch@gmail.com>
2010-05-06 17:48:56 -0700

    Trying to get mapreduce to work.

--- src/radlab/rain/util/MapReduceTransport.java
+++ src/radlab/rain/util/MapReduceTransport.java
@@ -21,7 +21,7 @@ import org.apache.hadoop.util.ToolRunner;
 
 
 @SuppressWarnings("deprecation")
-public class MapReduceTransport implements Tool
+public class MapReduceTransport
 {
 	private String _jobTrackerAddress = "";
 	private int _jobTrackerPort = 0;
@@ -41,9 +41,8 @@ public class MapReduceTransport implements Tool
 	public RunningJob submitJob( Configuration jobConf ) throws IOException
 	{
 		JobClient client = new JobClient( new InetSocketAddress( this._jobTrackerAddress, this._jobTrackerPort ), jobConf );
-		client.submitJob( (JobConf) jobConf );
+		return client.submitJob( (JobConf) jobConf );
 		
-		return null;
 	}
 	
 	/**
@@ -51,78 +50,9 @@ public class MapReduceTransport implements Tool
 	 */
 	public static void main( String[] args ) throws Exception
 	{
-		int res = ToolRunner.run(new Configuration(), new MapReduceTransport("localhost", 9001), args);
-	    System.exit(res);
-
-	}
-	
-	public int run(String[] args) throws Exception {
-	    if (args.length < 3) {
-	      System.out.println("Grep <inDir> <outDir> <regex> [<group>]");
-	      ToolRunner.printGenericCommandUsage(System.out);
-	      return -1;
-	    }
-
-	    Path tempDir =
-	      new Path("grep-temp-"+
-	          Integer.toString(new Random().nextInt(Integer.MAX_VALUE)));
-
-	    JobConf grepJob = new JobConf(new Configuration(), MapReduceTransport.class);
-
-	    try {
-
-	      grepJob.setJobName("grep-search");
-
-	      FileInputFormat.setInputPaths(grepJob, args[0]);
-
-	      grepJob.setMapperClass(RegexMapper.class);
-	      grepJob.set("mapred.mapper.regex", args[2]);
-	      if (args.length == 4)
-	        grepJob.set("mapred.mapper.regex.group", args[3]);
-
-	      grepJob.setCombinerClass(LongSumReducer.class);
-	      grepJob.setReducerClass(LongSumReducer.class);
-
-	      FileOutputFormat.setOutputPath(grepJob, tempDir);
-	      grepJob.setOutputFormat(SequenceFileOutputFormat.class);
-	      grepJob.setOutputKeyClass(Text.class);
-	      grepJob.setOutputValueClass(LongWritable.class);
-
-//	      JobClient.runJob(grepJob);
-	      this.submitJob(grepJob);
-
-	      JobConf sortJob = new JobConf(MapReduceTransport.class);
-	      sortJob.setJobName("grep-sort");
-
-	      FileInputFormat.setInputPaths(sortJob, tempDir);
-	      sortJob.setInputFormat(SequenceFileInputFormat.class);
-
-	      sortJob.setMapperClass(InverseMapper.class);
-
-	      sortJob.setNumReduceTasks(1);                 // write a single file                 
-	      FileOutputFormat.setOutputPath(sortJob, new Path(args[1]));
-	      sortJob.setOutputKeyComparatorClass           // sort by decreasing freq             
-	      (LongWritable.DecreasingComparator.class);
-
-//	      JobClient.runJob(sortJob);
-	      this.submitJob(sortJob);
-	    }
-	    finally {
-	      FileSystem.get(grepJob).delete(tempDir, true);
-	    }
-	    return 0;
-	  }
-
-	@Override
-	public Configuration getConf() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setConf(Configuration arg0) {
-		// TODO Auto-generated method stub
 		
+		GrepRain.main(args);
+
 	}
 	
 }
