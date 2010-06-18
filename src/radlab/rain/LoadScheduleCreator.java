27f1a0d241f3b4b747c0df3885fc39ed8e5ea515
rean <rean@eecs.berkeley.edu>
2010-06-17 20:46:31 -0700

    Explicitly added abstract definition of createSchedule to LoadScheduleCreator.

--- src/radlab/rain/LoadScheduleCreator.java
+++ src/radlab/rain/LoadScheduleCreator.java
@@ -1,8 +1,10 @@
 package radlab.rain;
 
+import java.util.LinkedList;
+
 //import java.util.LinkedList;
 
 public abstract class LoadScheduleCreator implements ILoadScheduleCreator 
 {
-
+	public abstract LinkedList<LoadProfile> createSchedule(); 
 }
