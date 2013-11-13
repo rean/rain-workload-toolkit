5416a58faa4f8591a44df03cee4f93bb54936ba1
sguazt <marco.guazzone@gmail.com>
2013-11-13 15:40:41 +0100

    [rubis] (bug-fix:minor) Make sure that a valid trace record object does exist

--- src/radlab/rain/workload/rubis/RubisOperation.java
+++ src/radlab/rain/workload/rubis/RubisOperation.java
@@ -168,7 +168,7 @@ public abstract class RubisOperation extends Operation
 	protected String getLastRequest()
 	{
 		final TraceRecord trace = this.getTrace();
-		if (trace._lstRequests == null || trace._lstRequests.isEmpty())
+		if (trace == null || trace._lstRequests == null || trace._lstRequests.isEmpty())
 		{
 			return null;
 		}
